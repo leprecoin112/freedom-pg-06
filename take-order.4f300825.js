6612fc90b4eac7d146a36774a1a62446a550ae97
leprecoin112 <36137472+leprecoin112@users.noreply.github.com>
2022-09-12 13:14:25 +0000

    Deploying to gh-pages from @ leprecoin112/freedom-pg-06@d5757650765f224ac04c68697ea9a3892ad10ae0 🚀

---
+++ take-order.4f300825.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-phone-open]"),closeMenuBtn:document.querySelector("[data-menu-phone-close]"),menu:document.querySelector("[data-phone-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=take-order.4f300825.js.map
---
+++ take-order.d67a8a9c.js
@@ -0,0 +1,2 @@
+(()=>{const e={openMenuBtn:document.querySelector("[data-menu-phone-open]"),closeMenuBtn:document.querySelector("[data-menu-phone-close]"),menu:document.querySelector("[data-phone-menu]")};function n(){e.menu.classList.toggle("is-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
+//# sourceMappingURL=take-order.d67a8a9c.js.map
